8bfacf5558c98eff9177b164ba68e24c0316e15f
Cygnuxxs <ashok7075657409@gmail.com>
2023-09-23 15:20:03 +0530

    Initial commit

---
+++ forms.py
@@ -0,0 +1,68 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, DateField, SelectField, BooleanField, FieldList, FormField
+
+sections_list = [("K1CSD", "K1CSD"),
+                 ("K1CSM", "K1CSM"),
+                 ("K1CAI", "K1CAI"),
+                 ("K1CSC", "K1CSC"),
+                 ("K1AID", "K1AID"),
+                 ("K2CSD", "K2CSD"),
+                 ("K2CSM", "K2CSM"),
+                 ("K2CAI", "K2CAI"),
+                 ("K2CSC", "K2CSC"),
+                 ("K2AID", "K2AID")]
+
+staff_list = [
+            ("Uday", "Uday"),
+            ("Sowjanya", "Sowjanya"),
+            ("Anitha", "Anitha"),
+            ("Sushmitha", "Sushmitha"),
+            ("Sujitha", "Sujitha"),
+            ("Raju", "Raju"),
+            ("Ramakrishna", "Ramakrishna"),
+            ("Veeramani", "Veeramani"),
+            ("Veeralakshmi", "Veeralakshmi"),
+            ("Khan", "Khan"),
+            ("Rohini", "Rohini"),
+            ("Suresh", "Suresh"),
+            ("Tirupathi Rao (LL)", "Tirupathi Rao (LL)"),
+            ("Ganesh (LL)", "Ganesh (LL)"),
+            ("Mani Kumar (LL)", "Mani Kumar (LL)"),
+            ("Others", "Others")
+]
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+hours = [1,2,3,4]
+
+class GetStudentForm(FlaskForm):
+    student_id = StringField("Student Roll Number", [DataRequired(), Length(min=10, max=10)])
+    get_details = SubmitField("Get Details")
+
+class AddDetailsForm(FlaskForm):
+    student_id = StringField("Student Roll Number", [DataRequired(), Length(min=10, max=10)])
+    first_name = StringField("First Name", [DataRequired(), Length(max=50)])
+    last_name = StringField("Last Name", [DataRequired(), Length(max=50)])
+    dob = DateField("Date of Birth", format = "%d-%m-%Y", validators=[DataRequired()], render_kw = {"type":"text"})
+    section = SelectField("Section", validators=[DataRequired()], choices=sections_list)
+    add_details = SubmitField("Add Details")
+
+class SectionForm(FlaskForm):
+    sections = SelectField("Sections", choices=sections_list, validators=[DataRequired()])
+    date = DateField("Date of Attendance", format = "%d-%m-%Y", validators=[DataRequired()], render_kw = {"type":"text"})
+    submit = SubmitField("Submit")
+
+class StaffForm(FlaskForm):
+    day = SelectField("Day", choices=days, validators=[DataRequired()])
+    hour = SelectField("Hour", choices=hours, validators=[DataRequired()])
+    subject = StringField("Subject Name", [DataRequired()])
+    staff = SelectField("Faculty Name", validators=[DataRequired()], choices=staff_list)
+    section = SelectField("Section", choices=sections_list, validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class AttendanceForm(FlaskForm):
+    confirm = BooleanField("Confirm", [DataRequired()])
+    submit = SubmitField("Post Attendance")
+    
+
+
---
+++ main.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from forms import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "Cygnuxxs"
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__ = "students"
+    student_unique_id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.String(10), unique = True, nullable = False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    section = db.Column(db.String(6), nullable = False)
+
+class Attendance(db.Model):
+    __tablename__ = "attendance"
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.String(10), nullable = False)
+    date = db.Column(db.Date, nullable =False)
+    status = db.Column(db.String(7), nullable = False)
+    section = db.Column(db.String(6), nullable = False)
+    __table_args__ = (db.UniqueConstraint('student_id', "date", name = "unique_student_date"),)
+
+class TimeTable(db.Model):
+    __tablename__ = "timetable"
+    id = db.Column(db.Integer, primary_key = True)
+    day = db.Column(db.String(10), nullable = False)
+    hour = db.Column(db.Integer, nullable = False)
+    subject = db.Column(db.String(50), nullable = False)
+    staff = db.Column(db.String(50), nullable = False)
+    section = db.Column(db.String(6), nullable = False)
+    __table_args__ = (db.UniqueConstraint("day", "hour", "section", name = "unique_entry"),)
+
+with app.app_context() as con:
+    db.create_all()
+
+@app.route('/', methods = ['GET','POST'])
+def home():
+    form = GetStudentForm()
+    error = None
+    if form.validate_on_submit():
+        student_id = form.student_id.data
+        student = Student.query.filter_by(student_id = student_id.upper()).first()
+        if student:
+            return redirect(url_for("get_details", roll_num = student.student_id))
+        else:
+            error = "The entered Student ID is not found."
+        
+    return render_template('index.html', title = "Get Student Details", form = form, err = error)
+
+@app.route('/student/<roll_num>')
+def get_details(roll_num):
+    err = None
+    hours = None
+    student = Student.query.filter_by(student_id = roll_num).first()
+    if student:
+        hour1 = TimeTable.query.filter_by(section = student.section).filter_by(hour = 1).all()
+        hour2 = TimeTable.query.filter_by(section = student.section).filter_by(hour = 2).all()
+        hour3 = TimeTable.query.filter_by(section = student.section).filter_by(hour = 3).all()
+        hour4 = TimeTable.query.filter_by(section = student.section).filter_by(hour = 4).all()
+        if not (hour1 == [] or hour2 == [] or hour3 == [] or hour4 == []):
+            hours = [hour1, hour2, hour3, hour4]
+    else:
+        err = "The given student ID is not found in the database."
+    staff = TimeTable.query.filter_by(section = student.section).all()
+    staff_dict = {}
+    for i in staff:
+        if i.subject not in staff_dict:
+            staff_dict[i.subject] = i.staff
+    return render_template("student_details.html", staff = staff_dict, err = err, student = student, hours = hours)
+
+@app.route("/add-details", methods = ['GET', 'POST'])
+def add_details():
+    form = AddDetailsForm()
+    err = None
+    if form.validate_on_submit():
+        student_id = form.student_id.data.upper()
+        print(type(student_id))
+        student = Student.query.filter_by(student_id = student_id).first()
+        if student is None:
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            date_of_birth = form.dob.data
+            section = form.section.data
+
+            new_student = Student(
+                student_id = student_id,
+                first_name = first_name,
+                last_name = last_name,
+                date_of_birth = date_of_birth,
+                section = section
+            )
+            db.session.add(new_student)
+            db.session.commit()
+            err = f"{student_id} Student Details added Successfully."
+        else:
+            err = "The entered student ID is already in the database."
+    return render_template('add_details.html',title = "Add Student Details", form = form, err = err)
+
+@app.route("/sections", methods = ["GET", "POST"])
+def section():
+    err = None
+    data = None
+    aform = None
+    form = SectionForm()
+    if form.validate_on_submit():
+        section = form.sections.data
+        data = Student.query.filter_by(section = section).order_by(Student.student_id).all()
+        for student in data:
+            setattr(AttendanceForm, f"{student.student_id}", BooleanField("Present"))
+        aform = AttendanceForm()
+        if aform.validate_on_submit():
+            for field in aform:
+                if field.name != "submit" and field.name != "csrf_token" and field.name != "confirm":
+                    new_entry = Attendance(
+                        student_id = field.name,
+                        date = form.date.data,
+                        status = "Present" if field.data else "Absent",
+                        section = form.sections.data
+                    )
+                    try:
+                        db.session.add(new_entry)
+                        db.session.commit()
+                        err = "Attendance Posted Successfully."
+                    except IntegrityError:
+                        db.session.rollback()
+                        err = "Attendance already taken for this section."
+    return render_template("sections.html", err = err, aform = aform, form = form, title = "Section wise Attendance", data = data)
+
+@app.route("/add-staff", methods = ['GET', "POST"])
+def staff():
+    form = StaffForm()
+    err = None
+    if form.validate_on_submit():
+        day = form.day.data
+        hour = form.hour.data
+        subject = form.subject.data
+        mentor = form.staff.data
+        section = form.section.data
+        new_entry = TimeTable(
+            day = day,
+            hour = hour,
+            subject = subject,
+            staff = mentor,
+            section = section
+        )
+        try:
+            db.session.add(new_entry)
+            db.session.commit()
+            err = "Entries are successfully Updated."
+        except IntegrityError:
+            db.session.rollback()
+            err = "Entry is already in the database."
+    return render_template("add-staff.html", form = form, err = err)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
